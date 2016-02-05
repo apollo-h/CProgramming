bf84cd701a64b4352fde6d3ab47a70f263224837
apollo-h <pallob.nstu@gmail.com>
2016-02-05 21:01:43 +0100

    This is the main C program

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int number;
+	printf("Please enter anumber ");
+	scanf("%d",&number);
+	printf("you enterd %d ",number);
+	getchar();
+	return 0;
+}
